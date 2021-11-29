5b9f07f8ca5e787eb052d4585dc4b39a11dfdad1
Shashank-Marwade <81464365+Shashank-Marwade@users.noreply.github.com>
2021-11-29 16:29:31 +0530

    update clock.c

--- clock_angle/clock.c
+++ clock_angle/clock.c
@@ -11,26 +11,26 @@ double angleHM(int hr,int min)
     }
     return ang;
 }
-double angleMS(int hr,int min,int sec)
-{
-    float m =(min%60+(float)sec/360)*6;
-    float s= sec*0.1;
-    float ang=fabs(m-s);
-    if(ang>180)
-    {
-        ang=360-ang;
-    }
-    return ang;
-}
+// double angleMS(int hr,int min,int sec)
+// {
+//     float m =(min%60+(float)sec/360)*6;
+//     float s= sec*0.1;
+//     float ang=fabs(m-s);
+//     if(ang>180)
+//     {
+//         ang=360-ang;
+//     }
+//     return ang;
+// }
 void main()
 {
     int hr,min,sec;
-    // printf("Enter Time HH MM: ");
-    // scanf("%d %d",&hr,&min);
-    // printf("Angle is %f",angleHM(hr,min));
+    printf("Enter Time HH MM: ");
+    scanf("%d %d",&hr,&min);
+    printf("Angle is %f",angleHM(hr,min));
     
-    printf("Enter Time HH MM SS: ");
-    scanf("%d %d %d",&hr,&min,&sec);
-    printf("Angle is %f",angleMS(hr,min,sec));
+    //printf("Enter Time HH MM SS: ");
+    //scanf("%d %d %d",&hr,&min,&sec);
+    //printf("Angle is %f",angleMS(hr,min,sec));
 
 }
