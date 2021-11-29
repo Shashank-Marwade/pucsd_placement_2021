861be7421412bcf3a89a581d03640d0bdafd20ca
Shashank-Marwade <81464365+Shashank-Marwade@users.noreply.github.com>
2021-11-29 14:42:07 +0530

    source

---
+++ clock_angle/clock.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+double angleHM(int hr,int min)
+{
+    float h =(hr%12+(float)min/60)*30;
+    float m =min*6;
+    float ang=fabs(h-m);
+    if(ang>180)
+    {
+        ang=360-ang;
+    }
+    return ang;
+}
+double angleMS(int hr,int min,int sec)
+{
+    float m =(min%60+(float)sec/360)*6;
+    float s= sec*0.1;
+    float ang=fabs(m-s);
+    if(ang>180)
+    {
+        ang=360-ang;
+    }
+    return ang;
+}
+void main()
+{
+    int hr,min,sec;
+    // printf("Enter Time HH MM: ");
+    // scanf("%d %d",&hr,&min);
+    // printf("Angle is %f",angleHM(hr,min));
+    
+    printf("Enter Time HH MM SS: ");
+    scanf("%d %d %d",&hr,&min,&sec);
+    printf("Angle is %f",angleMS(hr,min,sec));
+
+}
